239119552cb29b39cee76f456292ec452b513f47
Ben M <makingspidersense@gmail.com>
2021-03-05 19:34:44 -0600

    Moved JS to components.js file

---
+++ js/components.js
@@ -0,0 +1,51 @@
+// A-frame Components
+
+
+// Remove sky element from environment component
+AFRAME.registerComponent('remove-env-sky', {
+    init: function () {
+        const defaultSky = document.querySelector("a-entity a-sky.environment");
+        defaultSky.remove();
+    }
+});
+
+
+// Dim the lights
+// Note: This is specifically for the "Dreams" environment.
+AFRAME.registerComponent('dim-lights', {
+    init: function () {
+        const defaultLights = document.querySelector("a-entity.env-dream a-entity.environment:nth-child(2)");
+        defaultLights.setAttribute("visible", false);
+    }
+});
+
+
+// Toggle Music
+AFRAME.registerComponent('music-toggle', {
+    init: function () {
+        const el = this.el;
+        // Reset Local Storage
+        localStorage.setItem('soundState', 'paused');
+        localStorage.removeItem('soundState');
+        // Speaker - Play/Pause Toggle
+        el.addEventListener('click', playPauseToggle);
+        function playPauseToggle() {
+            const soundState = localStorage.getItem('soundState');
+            const torus = document.querySelector('#ps-torus');
+            const cone = document.querySelector('#ps-cone');
+            if (soundState == "playing") {
+                console.log('was playing');
+                el.components.sound.pauseSound();
+                torus.setAttribute("material", "color: #53e4e1; opacity: .5;");
+                cone.setAttribute("material", "color: #53e4e1; opacity: .5;");
+                localStorage.setItem('soundState', 'paused');
+            } else {
+                console.log('was paused');
+                el.components.sound.playSound();
+                torus.setAttribute("material", "color: #53e4e1; opacity: 1;");
+                cone.setAttribute("material", "color: #53e4e1; opacity: 1;");
+                localStorage.setItem('soundState', 'playing');
+            }
+        }
+    }
+});
