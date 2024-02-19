fbe97422700c12037d31129fb248b991d5484e69
Ben M <makingspidersense@gmail.com>
2024-02-18 22:29:26 -0600

    Add twinkling stars animation to the scene

--- js/components.js
+++ js/components.js
@@ -361,6 +361,59 @@ AFRAME.registerComponent('blink-control', {
 });
 
 
+AFRAME.registerComponent('twinkling-stars', {
+    init: function () {
+        const starTemplate = document.querySelector('#standard-star-template');
+        // Append clones to each cluster
+        for (let i = 1; i <= 6; i++) {
+            let cluster = document.querySelector('#star-cluster-' + i);
+            let clone = starTemplate.cloneNode(true);
+            cluster.appendChild(clone);
+        }
+        // Start the animation loops for each cluster
+        for (let i = 1; i <= 6; i++) {
+            let cluster = document.querySelector('#star-cluster-' + i).querySelector('.standard-star');
+            setTimeout(() => {
+                this.setupStars(cluster, i);
+            }, 225 * (i - 1)); // Staggering initiation by 225ms
+        }
+    },
+    setupStars: function (cluster, clusterNumber) {
+        const stars = cluster.querySelectorAll('.star-position');
+        let currentStar = 0;
+        // Animate stars, looping forever
+        const animateStar = () => {
+            if (currentStar >= stars.length) {
+                currentStar = 0; // Reset to start the loop again
+            }
+            let star = stars[currentStar];
+            let starHead = star.querySelector('.star-head');
+            console.log(`Animating star ${currentStar + 1} in cluster ${clusterNumber}`);
+            this.animateStarHead(starHead);
+            // Wait a bit and move to next star after animation
+            setTimeout(() => {
+                // Remove the animation attribute to reset the star
+                starHead.removeAttribute('animation__fadein');
+                starHead.removeAttribute('animation__visible');
+                starHead.removeAttribute('animation__fadeout');
+                // Move to the next star
+                currentStar++;
+                animateStar();
+            }, 2000); // 500ms fade-in, 500ms hold, 500ms fade-out, 500ms wait
+        };
+        // Start the animation with the star corresponding to the cluster number
+        currentStar = (clusterNumber - 1) % stars.length;
+        animateStar();
+    },
+    animateStarHead: function (starHead) {
+        // Animation logic: Fade in, hold, fade out
+        starHead.setAttribute('animation__fadein', 'property: opacity; from: 0; to: 1; dur: 500');
+        starHead.setAttribute('animation__visible', 'property: opacity; to: 1; startEvents: animationcomplete__fadein; dur: 500');
+        starHead.setAttribute('animation__fadeout', 'property: opacity; from: 1; to: 0; startEvents: animationcomplete__visible; dur: 500');
+    }
+});
+
+
 // Trigger shooting star animations
 let clonesCreated = false; // Flag to track if clones are created
 function triggerShootingStars() {
