bef29085fb46da87effa26b196a541c4bac11dab
Ben M <makingspidersense@gmail.com>
2025-04-29 12:27:09 -0500

    Improve nav mesh and make it a static body

--- js/components.js
+++ js/components.js
@@ -935,3 +935,32 @@ AFRAME.registerComponent('arm-swing-movement', {
         // console.log('Debug Direction - Yaw (deg):', worldYaw, ' Vector:', sum.x.toFixed(3), sum.z.toFixed(3));
     },
 });
+
+
+/**
+ * Add Model After Load
+ *
+ * Description: This component loads a GLTF model and adds an ammo-shape to it after the model is loaded. It's a workaround to use Ammo.js physics with 3D models.
+ *
+ * Credit: Based on snippet from icurtis1 🙏 (https://github.com/n5ro/aframe-physics-system/issues/192)
+ *
+ */
+AFRAME.registerComponent('add-model-after-load', {
+    schema: {
+        model: { default: '' },
+        body: { type: 'string', default: 'dynamic' },
+        shape: { type: 'string', default: 'hull' },
+    },
+    init() {
+        const gltfModel = document.createElement('a-entity');
+        this.el.appendChild(gltfModel);
+        gltfModel.setAttribute('gltf-model', this.data.model);
+        gltfModel.setAttribute('shadow', { receive: false });
+        // Specify what type of ammo-body (dynamic, static, kinematic)
+        gltfModel.setAttribute('ammo-body', { type: this.data.body });
+        // Wait for model to load before adding ammo-shape (box, cylinder, sphere, capsule, cone, hull)
+        this.el.addEventListener('model-loaded', () => {
+            gltfModel.setAttribute('ammo-shape', { type: this.data.shape });
+        })
+    }
+});
